1212f2d98913420ea3a65e829fd876e22924b91d
Ammar Qammaz <ammarkov@gmail.com>
2011-10-17 22:58:17 +0300

    added master/client pages

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php  
-  function ServeSite($hostname,$last)
+  function ServeSite($hostname,$last,$password)
   { 
   	  $file=fopen("users/".$hostname,"r");
   	  $now = fgets($file);
@@ -7,10 +7,12 @@
      fclose($file); 
      $file_to_broadcast = $now ."&last=".$now;
      $next_index = "index.php?i=".$hostname."&last=".$now;
+     $pass_input = "";
+     if ( strlen($password)>1 ) { $pass_input="&password=".$password; }
      echo "<html> 
              <head><title>View Link Service for user ".$hostname."</title></head>
              <body>    
-              <iframe src=\"ticker.php?i=".$hostname."&last=".$now."\" width=\"100%\" height=\"50\"></iframe>
+              <iframe src=\"ticker.php?i=".$hostname.$pass_input."&last=".$now."\" width=\"100%\" height=\"50\"></iframe>
      
               <iframe   id=\"target\" name=\"target\"  src=\"".$now."\" width=\"100%\" height=\"100%\">
                  <p>Your browser does not support iframes.</p>
@@ -23,10 +25,11 @@
 
 if (isset($_GET['i'])) 
  { 
-      
+    $password = "";  
     $last_site = "#";
     if ( isset($_GET['last'])  ) { $last_site=$_GET['last']; } 
-    ServeSite($_GET['i'],$_GET['last']);
+    if ( isset($_GET['password'])  ) { $password=$_GET['password']; } 
+    ServeSite($_GET['i'],$last_site,$password);
  }
   else
 { echo "<html><body>No user provided</body></html>"; }
--- ticker.php
+++ ticker.php
@@ -1,6 +1,16 @@
 <?php 
-
-  function ServeTicker($hostname,$last)
+  
+  function UpdateSite($hostname,$new_site,$password)
+   {
+   	//echo " Command hostname ".$hostname." password ".$password." site ".$new_site;
+   	//$file=fopen("users/".$hostname,"w");
+   	$file=fopen("users/ammar","w");
+   	 fwrite($file,$new_site);
+   	fclose($file); 
+   }   
+  
+  
+  function ServeTicker($hostname,$last,$origin,$password)
   {
   	  $file=fopen("users/".$hostname,"r");
   	  $now = fgets($file);
@@ -12,45 +22,88 @@
      $tick_timer = 30;
      if (strcmp($now,"closed.html")==0) { $tick_timer = 1000; /*Lets go a little (300x) slower :P */ } 
      
-     echo  "<html> 
-             <head>
-              <meta http-equiv=\"refresh\" content=\"".$tick_timer.";url=ticker.php?i=".$hostname."&last=".$now."\"> 
-              <script type=\"text/javascript\">
-                function ticker_url()
-                     {
-	                    parent.document.getElementById('target').src=\"".$now."\";
-                     }		
-              </script>     
-            </head> 
-            
-              "; 
-              
-              
-     $body_arg = "";
-     $status = "";
-              
-     if (strcmp($now,$last)!=0)
-     {  
-       $body_arg = "onload=\"ticker_url();\" ";
-       $status = " <b>Different</b>"; // @ last was @".$last."@";       
-     } else  
-     {
-       $status = " Same ";             
-     }
+
+
+    $body_arg = "";
+    $status = "";
+    $head_code = "<meta http-equiv=\"refresh\" content=\"".$tick_timer.";url=ticker.php?i=".$hostname."&last=".$now."\"> 
+                    <script type=\"text/javascript\">
+                          function target_url()
+                             {
+	                           parent.document.getElementById('target').src=\"".$now."\";
+                             }		
+                    </script>";   
+    $form_code = "";
     
-    echo  " <body ".$body_arg." >
-              <form>
-                ViewLink URL : <input type=\"text\" name=\"viewlink\" size=80 value=\"".$now."\" />  ".$status."
-              </form>
+                        
+    if ( $origin==1 )
+      {   	
+         // THIS IS THE ORIGIN (MASTER) PAGE , THERE IS NOW NEED FOR REFRESHING HERE , BUT WE WANT TO UPDATE Target
+         $body_arg = "onload=\"target_url();\" ";
+         $form_code = "<form enctype=\"multipart/form-data\" action=\"ticker.php\" method=\"POST\" >
+                        <input type=\"hidden\" name=\"user_host\" value=\"".$hostname."\" />
+                        <input type=\"hidden\" name=\"pass_cookie\" value=\"".$password."\" />
+                        ViewLink ".$status." URL : <input type=\"text\" name=\"viewlink\" size=80 value=\"".$now."\" /> 
+                        <input type=\"submit\" value=\"Go\" name=\"submit\"  />        
+                        <a target=\"_blank\" href=\"".$next_index."\">Viewer Link</a>   
+                       </form>                     
+                       "; 
+      } else 
+      {  
+         if (strcmp($now,$last)!=0)
+          {  
+            $body_arg = "onload=\"target_url();\" ";
+            $status = "<b>(Different)</b>"; // @ last was @".$last."@";       
+          }                
+          
+        $form_code = "<form>ViewLink ".$status." URL : <input type=\"text\" name=\"viewlink\" size=80 value=\"".$now."\" /></form>";   
+      }      	    
+     
+     
+     echo  "<html>
+             <head>"
+               .$head_code."
+             </head> 
+             <body ".$body_arg." >
+               ".$form_code."
             </body>
            </html> ";     
   }
 
- 
+  
 
-if (isset($_GET['i'])&&isset($_GET['last'])) 
+
+/*
+   ///////////////////////////////////
+            NEW PAGE ( MASTER )
+   /////////////////////////////////// 
+*/
+if(isset($_POST['submit']))
+   {
+   	if (isset($_POST['user_host']) && isset($_POST['viewlink']) && isset($_POST['pass_cookie']) ) 
+   	 {
+         UpdateSite($_POST['user_host'],$_POST['viewlink'],$_POST['pass_cookie']);  
+   	   ServeTicker($_POST['user_host'],$_POST['viewlink'],1,$_POST['pass_cookie']);  	 
+   	 } else 
+   	 {
+   	 	echo "Not all post values provided";
+   	 }  
+   }
+ else  
+/*
+   ///////////////////////////////////
+               CLIENT PAGE 
+   /////////////////////////////////// 
+*/
+if ( isset($_GET['i']) && isset($_GET['last']) ) 
  {  
-  ServeTicker($_GET['i'],$_GET['last']);
+   if ( isset($_GET['password']) ) 
+     {  
+       ServeTicker($_GET['i'],$_GET['last'],1,$_GET['password']);
+     } else 
+     {
+       ServeTicker($_GET['i'],$_GET['last'],0,"");
+     }  
  }
   else
 { echo "Nothing provided"; }
